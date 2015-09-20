c358dd40303222b03867dcae023dd4cad070982d
Vipul Shah <vipulshah@deloitte.com>
2015-09-21 00:34:34 +0530

    Updated for Swift 2.0

--- MovieApp/MovieTableViewController.swift
+++ MovieApp/MovieTableViewController.swift
@@ -21,22 +21,24 @@ class MovieTableViewController: UITableViewController {
         
         let urlString = "https://api-v2launch.trakt.tv/movies/trending?page=1&limit=50&extended=full,images"
         
-        let manager = Alamofire.Manager.sharedInstance
+        let headers   = ["trakt-api-version":"2","trakt-api-key":"a0c9e1842e5b4bd2fa07bd814a13659b5e6561b9d556144a6ddb73c838844a6b"]
         
-        manager.session.configuration.HTTPAdditionalHeaders  = ["trakt-api-version":"2","trakt-api-key":"a0c9e1842e5b4bd2fa07bd814a13659b5e6561b9d556144a6ddb73c838844a6b"]
-        
-        let headers = ["Accept": "application/json"]
-        
-        manager.request(Method.GET, urlString, headers: headers).responseJSON() {
-            (_, _, data, _) in
-            self.parseMovieJson(SwiftyJSON.JSON(data!))
+        Alamofire.request(Method.GET, urlString,headers: headers).responseJSON {
+            (_,_,result) -> Void in
+            switch result {
+            case .Success(let data):
+                self.parseMovieJson(SwiftyJSON.JSON(data))
+            case .Failure(_, let error):
+                print((error as NSError).localizedDescription)
+            }
         }
     }
     
     func parseMovieJson(json:SwiftyJSON.JSON){
+        print(json.type)
         if let moviesArray = json.array {
             for movie in moviesArray {
-                var movieClass = Movie()
+                let movieClass = Movie()
                 if let movieParentDictonary = movie.dictionary {
                     if let movieInfo = movieParentDictonary["movie"]?.dictionary {
                         if let title = movieInfo["title"]?.string {
@@ -86,7 +88,11 @@ class MovieTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let movieCell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCellTableViewCell
         movieCell.movieTitle.text = movies[indexPath.row].title
-        movieCell.moviePosterImageView.hnk_setImageFromURL((NSURL(string: movies[indexPath.row].imageUrl!)!), placeholder: UIImage(named: "placeholder.png"))
+        
+        if let url = movies[indexPath.row].imageUrl {
+            movieCell.moviePosterImageView.hnk_setImageFromURL((NSURL(string:url)!), placeholder: UIImage(named: "placeholder.png"))
+        }
+        
         return movieCell
     }
     
